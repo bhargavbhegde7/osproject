9f86e6c971b6ff5182faba22ce3bb677da612345
bhargav <bhargavbhegde7@gmail.com>
2016-11-07 23:25:45 +0530

    reading frin the disk boot_sect_7.asm empty file added

--- src/boot_sect_6.asm
+++ src/boot_sect_6.asm
@@ -1,20 +1,41 @@
 mov ah, 0x0e
 
+mov al, '1'
+int 0x10
+mov al, '-'
+int 0x10
 mov al, [the_secret]
+int 0x10
+;---------------------------
+mov al, '2'
+int 0x10
+mov al, '-'
 int 0x10
 
 mov bx, 0x7c0
 mov ds, bx
 mov al, [the_secret]
 int 0x10
+;----------------------------
+mov al, '3'
+int 0x10
+mov al, '-'
+int 0x10
 
 mov al, [es:the_secret]
+int 0x10
+;------------------------------
+mov al, '4'
+int 0x10
+mov al, '-'
 int 0x10
 
 mov bx, 0x7c0
 mov es, bx
 mov al, [es:the_secret]
 int 0x10
+
+;--------------------------------
 
 jmp $
 
